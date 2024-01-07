311fff2f0ee56526ee3ad821771e3d3b1f5ac327
akindunjoyetolulope <akindunjoyetolulope5@gmail.com>
2024-01-07 01:29:46 +0100

    resp

--- src/styles/media.ts
+++ src/styles/media.ts
@@ -3,7 +3,7 @@ const mq = (bp: number) => `@media (max-width: ${bp}px)`;
 
 const media = {
   smallMobile: mq(320), // iphone 5/SE 😂
-  mobile: mq(480), // mobile
+  mobile: mq(600), // mobile
   tablet: mq(768), // tablets
   smallDesktop: mq(1024), // tablets landscape, small desktops
 };
